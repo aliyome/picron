ac86452702a91a273462dd0dfa5e26203e182987
aliyome <aliyome@gmail.com>
2021-05-01 11:29:06 +0900

    feat: throw error when initial hint is invalid

--- src/lib/models/puzzle.spec.ts
+++ src/lib/models/puzzle.spec.ts
@@ -1,1 +1,7 @@
 import { init } from './puzzle';
+
+describe('init', () => {
+  it('throws an error when data is invalid', () => {
+    expect(() => init({ rows: [[0]], columns: [[1]] })).toThrow();
+  });
+});
--- src/lib/models/puzzle.ts
+++ src/lib/models/puzzle.ts
@@ -1,7 +1,15 @@
+import { sum } from '../helpers/array';
+
 const initLineHint = (hint: Hint): Hint =>
   hint.map((h) => (h.length === 1 && h[0] === 0 ? [] : [...h]));
 
+const isValidHint = (rows: Hint, columns: Hint) => sum(rows.map(sum)) === sum(columns.map(sum));
+
 export function init(data: PuzzleData): Puzzle {
+  if (!isValidHint(data.rows, data.columns)) {
+    throw new Error('hint is not valid');
+  }
+
   return {
     width: data.columns.length,
     height: data.rows.length,
