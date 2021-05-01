8097210ce0e7f07e57286b51d8ed30cf5636d1ec
aliyome <aliyome@gmail.com>
2021-05-01 11:55:06 +0900

    feat: add test on initializing puzzle

--- src/lib/models/puzzle.spec.ts
+++ src/lib/models/puzzle.spec.ts
@@ -4,4 +4,12 @@ describe('init', () => {
   it('throws an error when data is invalid', () => {
     expect(() => init({ rows: [[0]], columns: [[1]] })).toThrow();
   });
+
+  it('makes hint array blank when hint is [0]', () => {
+    const puzzle = init({ rows: [[0], [1]], columns: [[1], [0]] });
+    expect(puzzle.hint).toEqual({
+      rows: [[], [1]],
+      columns: [[1], []],
+    });
+  });
 });
