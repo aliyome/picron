25a5a641944f23c808d4c27f1c9220b0a039c03f
aliyome <aliyome@gmail.com>
2021-05-01 19:03:02 +0900

    feat: add test

--- src/lib/models/line.spec.ts
+++ src/lib/models/line.spec.ts
@@ -2,7 +2,7 @@ import { cellType } from '../helpers/const';
 import { isValid, makeLine } from './line';
 
 describe('Line isValid', () => {
-  describe('1x1 [0]', () => {
+  describe('[0]', () => {
     const line = makeLine(1, []);
     it('valid', () => {
       line.set(0, cellType.Empty);
@@ -14,7 +14,7 @@ describe('Line isValid', () => {
     });
   });
 
-  describe('1x1 [1]', () => {
+  describe('[1]', () => {
     const line = makeLine(1, [1]);
     it('valid', () => {
       line.set(0, cellType.Checked);
@@ -22,6 +22,25 @@ describe('Line isValid', () => {
     });
     it('invalid', () => {
       line.set(0, cellType.Empty);
+      expect(isValid(line)).toBeFalsy();
+    });
+  });
+
+  describe('[0, 1, 1, 0, 1, 1, 1, 0, 1]', () => {
+    const setTestState = (line: Line) => {
+      [0, 1, 1, 0, 1, 1, 1, 0, 1].forEach((value, i) => {
+        line.set(i, value as Cell);
+      });
+    };
+    it('valid', () => {
+      const line = makeLine(9, [2, 3, 1]);
+      setTestState(line);
+      expect(isValid(line)).toBeTruthy();
+    });
+
+    it('invalid', () => {
+      const line = makeLine(9, [2, 4, 1]);
+      line.state = [0, 1, 1, 0, 1, 1, 1, 0, 1];
       expect(isValid(line)).toBeFalsy();
     });
   });
